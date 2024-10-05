c0191c5e140b32d1a8ed729c162dcd11996f736c
Ho Seok <swiftie52@naver.com>
2024-10-05 15:52:38 +0900

    request param 까지

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -11,34 +11,34 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @RestController
-
+@RequestMapping("/mapping/users") // 중복되는 경로는 컨트롤러 공통으로 처리
 public class MappingClassController {
     // 회원 목록 조회
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String users(){
         return "get users";
     }
     // 회원 등록
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser()
     {
         return "post user";
     }
     // 개별 회원 조회
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable("userId") String userId)
     {
         return "get userId="+userId;
     }
     // 회원 정보 수정
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("/{userId}")
     public String updateUser(@PathVariable("userId") String userId)
     {
         return "update userId="+userId;
     }
 
     // 회원 삭제
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable("userId") String userId)
     {
         return "delete userId="+userId;
